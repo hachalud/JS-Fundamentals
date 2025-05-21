c07989f00be518c3fdccfca4c4cc9a2e7c831c41
Hachalu Diriba <hachaludiriba69@gmail.com>
2025-05-21 11:36:25 +0300

    Loop to languages

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+let result = "";
+for (let i = 0; i < 3; i++) {
+  result += lines[i] + "\n";
+}
+
+console.log(result.trim());
